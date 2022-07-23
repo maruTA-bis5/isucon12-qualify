3f2f70a69670b176d0e495e511271b18e0efc587
Takayuki Maruyama <bis5.wsys@gmail.com>
2022-07-23 02:42:00 +0000

    specify initlal data dir

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -8,6 +8,7 @@ ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
 ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
+ISUCON_DB_INIT_DIR=${ISUCON_DB_INIT_DIR:-/home/isucon/initial_data}
 
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
@@ -18,4 +19,4 @@ mysql -u"$ISUCON_DB_USER" \
 
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
-cp -r ../../initial_data/*.db ../tenant_db/
+cp -r $ISUCON_DB_INIT_DIR/*.db ../tenant_db/
