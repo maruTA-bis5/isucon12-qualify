5317968d0dffab572bf6464c4ebb495694020b1e
Takayuki Maruyama <bis5.wsys@gmail.com>
2022-07-23 16:04:04 +0900

    trial: ranking: create data without file lock

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1398,11 +1398,11 @@ func competitionRankingHandler(c echo.Context) error {
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(ctx, v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
+	// fl, err := flockByTenantID(ctx, v.tenantID)
+	// if err != nil {
+	// 	return fmt.Errorf("error flockByTenantID: %w", err)
+	// }
+	// defer fl.Close()
 	pss := []RankingPlayerScoreRow{}
 	if err := tenantDB.SelectContext(
 		ctx,
