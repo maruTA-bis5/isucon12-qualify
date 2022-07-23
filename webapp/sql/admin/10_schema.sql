919e9dddd353869ae4abc48ac58d40426aee6122
Takayuki Maruyama <bis5.wsys@gmail.com>
2022-07-23 06:07:07 +0000

    create visit_history_all_ids_idx

--- webapp/sql/admin/10_schema.sql
+++ webapp/sql/admin/10_schema.sql
@@ -29,3 +29,5 @@ CREATE TABLE `visit_history` (
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE INDEX visit_history_all_ids_idx ON visit_history(tenant_id, competition_id, player_id);
